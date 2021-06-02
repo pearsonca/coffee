69c39b67211644e3fdfab2606440f1eca76b7d19
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2021-06-02 12:17:20 +0100

    combine cq and cq functions h/t @pearsonca

--- R/cq.R
+++ R/cq.R
@@ -1,25 +1,20 @@
 #' Easy quote characters
 #'
-#' @param ... Unquoted variables (seperated by commas) that you wish to quote
 #' @param x Character vector which you wish to split on spaces
+#' @param ... Unquoted variables (seperated by commas) that you wish to quote
 #'
 #' @importFrom utils capture.output
 #' @export
-cq <- function(...) {
-    x <- substitute(list(...))
-    x <- vapply(x, deparse, character(1))
-    res <- x[-1]
-    tmp <- capture.output(dput(res, control = "all"))
-    clipr::write_clip(tmp)
-    res
-}
-
-#' @rdname cq
-#' @export
-ccq <- function(x) {
-  res <- sub("^[[:space:]]+", "", x)
-  res <- strsplit(res, "[[:space:]]+")
-  if (length(res) == 1L) res <- unlist(res)
+cq <- function(x, ...) {
+  if (is.character(substitute(x))) {
+    res <- trimws(x)
+    res <- strsplit(res, "[[:space:]]+")
+    if (length(res) == 1L) res <- unlist(res)
+  } else {
+    pr <- substitute(list(x, ...))
+    pr <- vapply(pr, deparse, character(1))
+    res <- pr[-1]
+  }
   tmp <- capture.output(dput(res, control = "all"))
   clipr::write_clip(tmp)
   res
